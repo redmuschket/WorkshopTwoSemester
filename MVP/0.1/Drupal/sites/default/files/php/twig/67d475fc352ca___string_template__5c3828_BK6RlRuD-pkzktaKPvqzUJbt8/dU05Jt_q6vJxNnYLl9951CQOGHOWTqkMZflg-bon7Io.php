f489782521c547e89a09891f9d1831743368fe5b
RedHead <93613196+redmuschket@users.noreply.github.com>
2025-03-23 13:02:34 +0500

    Drupal add

---
+++ MVP/0.1/Drupal/sites/default/files/php/twig/67d475fc352ca___string_template__5c3828_BK6RlRuD-pkzktaKPvqzUJbt8/dU05Jt_q6vJxNnYLl9951CQOGHOWTqkMZflg-bon7Io.php
@@ -0,0 +1,97 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* __string_template__5c38282207cba133672b4b024f320ac4 */
+class __TwigTemplate_52767694a5606e7cbac7b1f57f00acf6 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 1
+        yield "сообщение";
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "__string_template__5c38282207cba133672b4b024f320ac4";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  44 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "__string_template__5c38282207cba133672b4b024f320ac4", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = [];
+        static $filters = [];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ MVP/0.1/Laravel/resources/views/presentation.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Деталь</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        .detail {
+            border: 1px solid #ccc;
+            padding: 20px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+        .detail h2 {
+            margin-top: 0;
+        }
+    </style>
+</head>
+<body>
+
+<div class="detail">
+    <h2>Название детали</h2>
+    <p>Описание детали</p>
+</div>
+
+</body>
+</html>
